fc9d5051011f6a025f18d4e4d527929be233932c
mohamed <mohamedzaatouti@gmail.com>
2022-05-16 13:44:32 +0200

    make it better

--- Config/ConnectDB.js
+++ Config/ConnectDB.js
@@ -1,12 +1,11 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
-const ConnectDB = async() =>  { 
-try{
-    await mongoose.connect(process.env.MONGO_URI)
-    console.log('DB is connected')
-}
-catch (error) { 
-    console.log('DB is NOT connected')
-}
-}
-module.exports = ConnectDB
+const ConnectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true });
+    console.log("DB is connected");
+  } catch (error) {
+    console.log("DB is NOT connected");
+  }
+};
+module.exports = ConnectDB;
--- Server.js
+++ Server.js
@@ -12,7 +12,17 @@ app.use("/api/userAuth", userRouter);
 app.use("/api/product", productRouter);
 // app.use("/api/cart", productRouter);
 
+const path = require("path");
+
+// ... other app.use middleware
+app.use(express.static(path.join(__dirname, "client", "build")));
+
+// Right before your app.listen(), add this:
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
+
 app.listen(
-  process.env.port,
-  console.log(`Server is running on the port ${process.env.port}`)
+  process.env.PORT || 5000,
+  console.log(`le serveur est connectÃ© sur le port ${process.env.port}`)
 );
